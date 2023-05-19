af400b3cfd8f139e4bf33fcbc7959dda246b3e96
alexis <al222111377@gmail.com>
2023-05-19 12:44:48 -0600

    index utilizando el componente p

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,10 +4,10 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <p :letras"A" valor:{{ $letra }}>
-                    <p :letras"B" valor:{{ $letra }}>
-                    <p :letras"C" valor:{{ $letra }}>
-                    <p :letras"D  valor:{{ $letra }}>
+                    <p> valor:{{ $letra }}</p>
+                    <p> valor:{{ $letra }}</p>
+                    <p> valor:{{ $letra }}</p>
+                    <p> valor:{{ $letra }}</p>
                 </div>
             </div>
         </div>

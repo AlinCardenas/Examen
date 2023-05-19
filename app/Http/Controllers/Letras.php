d3cfd2c89bae8bce4439ffd2c6076beb54696222
Alin Cardenas <al222110745@gmail.com>
2023-05-19 11:23:29 -0600

    Llamar info de la bd

--- app/Http/Controllers/Letras.php
+++ app/Http/Controllers/Letras.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Content;
 use Illuminate\Http\Request;
 
 class Letras extends Controller
 {
     public function index(){
+        $palabra=  Content::all('word');
         return view('index');
     }
+
 }

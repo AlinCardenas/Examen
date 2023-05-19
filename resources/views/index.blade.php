496e48a15d9224d20eb10fed13347b9268055eaa
NelsonCejudo <nelsoncejudo12@gmail.com>
2023-05-19 11:49:26 -0600

    coso
    pd

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,13 @@
 
-<x-guest-layout>
-    <div class="text-whit">
-            {{ $palabra->word }}
-    </div>
-</x-guest-layout>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+{{ $palabra->word }}
+</body>
+</html>

fd9134f2d9ee5f98c8b5a16a77525bf17dc8a095
alexis <al222111377@gmail.com>
2023-05-19 12:01:39 -0600

    letras

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,13 +1,14 @@
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-{{ $palabra->word }}
-</body>
-</html>
+<x-guest-layout>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    Letra: {{ $letra }}
+                </div>
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    Letra: {{ $letra }}
+                </div>
+            </div>
+        </div>
+    </div>
+</x-guest-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,34 +1,13 @@
 <?php
 
-use App\Http\Controllers\Letras;
+use App\Http\Controllers\ApiController;
+use App\Http\Controllers\BaseController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TripsController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/letra', function () {
+    $letra = 'A'; // Letra que deseas mostrar
+    return view('index', compact('letra'));
 });
-
-Route::get('/letra',[Letras::class, 'index']);
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-require __DIR__.'/auth.php';

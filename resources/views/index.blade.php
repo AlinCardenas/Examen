930311ce26fb05922d0766baa246e9678589faef
alexis <al222111377@gmail.com>
2023-05-19 12:38:04 -0600

    componente p

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,10 +3,10 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    Letra: {{ $letra }}
-                </div>
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    Letra: {{ $letra }}
+                    <p :letras"A" valor:{{ $letra }}>
+                    <p :letras"B" valor:{{ $letra }}>
+                    <p :letras"C" valor:{{ $letra }}>
+                    <p :letras"D  valor:{{ $letra }}>
                 </div>
             </div>
         </div>

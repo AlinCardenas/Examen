a1cff9ba3fb4fb1a1f1211e8ee60de3a00d05b54
NelsonCejudo <nelsoncejudo12@gmail.com>
2023-05-19 12:25:08 -0600

    vistalimpia
    vista limpia

--- app/Http/Controllers/Letras.php
+++ app/Http/Controllers/Letras.php
@@ -8,17 +8,10 @@ use Illuminate\Http\Request;
 class Letras extends Controller
 {
     public function index(){
-        $palabra=  Content::first();   
-        
-        $letras = str_split($palabra);
+        $palabra=  Content::all('word');
+        $letra= strtr($palabreqa); 
 
-// Asigna cada letra a una variable
-$letra1 = $letras[0];
-$letra2 = $letras[1];
-$letra3 = $letras[2];
-$letra4 = $letras[3];
-
-        return view('index', compact("palabra"));
+        return view('index');
     }
 
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,23 +1,9 @@
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    @php
-    // Esto separa la letra especifica
-        $letra = str_replace('DR5', '', $palabra->word);
-    @endphp
-    <p>Letra 1:</p>
-    {{ $letra }}
-    {{$letras = str_split($palabra);}}
-    {{$letra1 = $letras[0];}}
-{{$letra2 = $letras[1];}}
-{{$letra3 = $letras[2];}}
-{{$letra4 = $letras[3];}}
-</body>
-</html>
+<x-guest-layout>
+    <div class="text-whit">
+            {{ $palabra->word }}
+    </div>
+    <div>
+        hola
+    </div>
+</x-guest-layout>
